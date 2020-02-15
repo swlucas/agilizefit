597ab601cc8c9db5cd6e22cc52dc8bb10dcdb658
Eliezer Marques <neto15.cg@gmail.com>
2020-02-15 10:52:42 -0300

    Create component of camera

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import Camera from "./components/Camera";
 
 const Main = () => {
+  const [modaVisible, setModalVisible] = useState(false);
   return (
-    <View>
-      <Camera />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Camera
+        visible={modaVisible}
+        onTakePhoto={() => console.log("XXXXXXX")}
+        onRequestClose={() => setModalVisible(false)}
+      />
+
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Text>Abrir câmera</Text>
+      </TouchableOpacity>
     </View>
   );
 };
