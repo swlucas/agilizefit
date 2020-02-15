b900ef6a0066bf0fe0eeb18697b4dabf3801910c
Eliezer Marques <neto15.cg@gmail.com>
2020-02-15 11:11:28 -0300

    Resolve conflicts

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
-import Header from './components/Header';
-import Camera from './components/Camera';
-import Classification from './components/Classification';
-import Season from './components/Season';
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import Header from "./components/Header";
+import Classification from "./components/Classification";
+import Season from "./components/Season";
 import ScrollHorizontal from "./components/ScrollHorizontal";
 import Camera from "./components/Camera";
 
 const Main = () => {
   const [modaVisible, setModalVisible] = useState(false);
   return (
-    <SafeAreaView>
-      <Header />
-      <Classification />
-      <Season />
-    </SafeAreaView>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Camera
-        visible={modaVisible}
-        onTakePhoto={() => console.log("XXXXXXX")}
-        onRequestClose={() => setModalVisible(false)}
-      />
-
-      <ScrollHorizontal></ScrollHorizontal>
-
-      <TouchableOpacity onPress={() => setModalVisible(true)}>
-        <Text>Abrir câmera</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <SafeAreaView>
+        <Header />
+        <Classification />
+        <Season />
+        <Camera
+          visible={modaVisible}
+          onTakePhoto={() => console.log("XXXXXXX")}
+          onRequestClose={() => setModalVisible(false)}
+        />
+        <ScrollHorizontal></ScrollHorizontal>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <Text>Abrir câmera</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
   );
 };
 
